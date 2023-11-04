cd34a370d68332ae3fbaf591953ba68ec7d95548
carlmstott <cstott@utexas.edu>
2023-11-03 22:19:43 -0500

    code is updated to reflct goor OOp practices, but I'm experiancing a bug where the object gets pulled twords the particles instead of away

--- sim_trial1.py
+++ sim_trial1.py
@@ -29,36 +29,38 @@ target_pos = np.array([5* WIDTH // 6, HEIGHT // 2])
 
 #making our particle object
 class particle:
-    def __init__(self, position, particleForce=0, raduis=5):
+    def __init__(self, position, raduis=5, velocity=0, force=0):
         self.position=position
-        self.particleForce=particleForce #the force acting on the particle, could be from the object or other particles
+        self.force=force #the force acting on the particle
         self.radius=raduis
+        self.velocity = velocity
+    
+    #making moveing a method of the particle object, takes in current position
+    #point we want to move to, and speed we want to move tword it
+    def move_towards(self, point2, speed):
+        """Move point1 towards point2 by a certain speed."""
+        direction = (point2 - self.position)
+        direction_norm = np.linalg.norm(direction)
+        if direction_norm == 0:  # To prevent division by zero
+            return self.position
+        direction = direction / direction_norm
+        newPose=self.position + direction * speed #add this to a particles position to update its location
+        self.position=newPose
+        
+    #maybe making a collide function here is a good idea, unsure as of now
 
-#making our object object... maybe this could just inherit from the particle class... for the future
-class oject:
-    def __init__(self, position=object_pos, objectForce=0, raduis = object_radius, velocity = 0):
-        self.position=position
-        self.objectForce=objectForce
-        self.radius=raduis
-        self.velocity = 0
+#this is the thing we want to move tword the goal
+object=particle(position=object_pos, raduis=object_radius)
 
-#creating a list of 20 particle objects all with random positions
+#for now the cursor is treated like a particle so we can play around with physics, will remove later
+cursor=particle(position=np.array(pygame.mouse.get_pos()))
+
+#creating a list of 20 particle objects all with random initial positions
 particle_list=[]
 n_particles = 20
 for i in range (0,n_particles):
-    instence=particle(position=np.random.rand(1, 2) * [WIDTH, HEIGHT])
+    instence=particle(position=np.random.rand(2) * [WIDTH, HEIGHT])
     particle_list.append(instence)
-
-#function that moves the objects in out space
-def move_towards(point1, point2, speed=1.0):
-    """Move point1 towards point2 by a certain speed."""
-    direction = (point2 - point1)
-    direction_norm = np.linalg.norm(direction)
-    if direction_norm == 0:  # To prevent division by zero
-        return point1
-    direction = direction / direction_norm
-    return point1 + direction * speed
-
 
 
 # Main loop
@@ -70,37 +72,48 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    # Get cursor position and treat it as a particle
-    cursor=particle(position=np.array(pygame.mouse.get_pos(), dtype=float))
+    # updating the cursor particles position
+    cursor.position=np.array(pygame.mouse.get_pos())
     
     # Particles influenceing object position
     # Calculate the collective force from particles in contact
+    
     collective_force = np.zeros(2)
     for particle in np.append(particle_list, cursor):  # Include cursor position in our particle list
         if np.linalg.norm(particle.position - object.position) <= contact_distance:
             #checking all particles that are in contact with the object
             # Particles push the object away from them, hence the negative sign
-            collective_force = (object.position - particle.position)
-            particle_force = (particle - object_pos)
-
+            collective_force = collective_force + (object.position - particle.position)
+            #if multiple particles are colliding with the object, their forces should act
+            #communitivly on the object
+            particle.force = (particle.position - object.position)
+    object.force=collective_force
+    
+    
     # Apply the collective force to move the object
     if np.linalg.norm(collective_force) > 0:
-        # The object's speed is proportional to the number of particles in contact,
-        #lets change it to be 1/4 as proportonal, as the perticles are 1/4 the size of the object
-        object_pos += 1/4*(collective_force * (1 / np.linalg.norm(collective_force)))
+        #unit vector in direction of collective force
+        force_direction=collective_force * (1 / np.linalg.norm(collective_force))
+        #making force magnitude of the particles 1/4 as effective, the particles are 1/4 the size of ght object
+        force_magnitude=(1/4) * collective_force
+        object.move_towards(force_direction, force_magnitude)
+        #object.position = object.position + 1/4*(collective_force * (1 / np.linalg.norm(collective_force)))
 
 
     # Update particle positions (just random movement) -> Eventually controlled by TF
-    for i in range(n_particles):
-        particle_positions[i] = move_towards(particle_positions[i], np.random.rand(2) * [WIDTH, HEIGHT], speed=1.0)
+    for particle in particle_list:
+        particle.move_towards(np.random.rand(2) * [WIDTH, HEIGHT], speed=1.0)
 
     # Draw everything
-    pygame.draw.circle(screen, BLUE, object_pos.astype(int), object_radius)
+    try:
+        pygame.draw.circle(screen, BLUE, center = (object.position[0], object.position[1]), radius=object.radius)
+    except:
+        print("check")
     pygame.draw.circle(screen, GREEN, target_pos, target_radius)
-    for particle in particle_positions:
-        pygame.draw.circle(screen, RED, particle.astype(int), particle_radius)
+    for particle in particle_list:
+        pygame.draw.circle(screen, RED, center = (particle.position[0], particle.position[1]), radius=particle.radius)
      # Draw the cursor particle
-    pygame.draw.circle(screen, BLACK, cursor_pos.astype(int), particle_radius)
+    pygame.draw.circle(screen, BLACK, cursor.position.astype(int), cursor.radius)
 
 
     pygame.display.flip()
